1a76b2c897145a9ce34d02ad2e707143d9a3c6d4
h13i32maru <h13i32maru@gmail.com>
2017-03-05 22:04:32 +0900

    fix(undocument): use new handler option

--- esdoc-undocumented-identifier-plugin/src/index.js
+++ esdoc-undocumented-identifier-plugin/src/index.js
@@ -1,12 +1,6 @@
 const UndocumentedIdentifierPlugin = require('./UndocumentedIdentifierPlugin');
 
-let option;
-
-exports.onStart = function(ev) {
-  option = ev.data.option;
-};
-
 exports.onHandleTag = function(ev) {
-  const plugin = new UndocumentedIdentifierPlugin(option);
+  const plugin = new UndocumentedIdentifierPlugin(ev.data.option);
   ev.data.tag = plugin.exec(ev.data.tag);
 };
