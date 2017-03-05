5482749fd4a9fc30bca31e1f8243e4f1858a1d9d
h13i32maru <h13i32maru@gmail.com>
2017-03-05 22:05:59 +0900

    fix(unexport): use new handler option

--- esdoc-unexported-identifier-plugin/src/index.js
+++ esdoc-unexported-identifier-plugin/src/index.js
@@ -1,12 +1,6 @@
 const UnexportedIdentifierPlugin = require('./UnexportedIdentifierPlugin');
 
-let option;
-
-exports.onStart = function(ev) {
-  option = ev.data.option;
-};
-
 exports.onHandleTag = function(ev) {
-  const plugin = new UnexportedIdentifierPlugin(option);
+  const plugin = new UnexportedIdentifierPlugin(ev.data.option);
   ev.data.tag = plugin.exec(ev.data.tag);
 };
