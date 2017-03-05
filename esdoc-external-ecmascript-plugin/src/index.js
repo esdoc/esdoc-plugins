018d6a41170577281139d25e18ee2b226ee7e8ef
h13i32maru <h13i32maru@gmail.com>
2017-03-05 21:03:13 +0900

    fix(external-ecmascript): use new handler option

--- esdoc-external-ecmascript-plugin/src/index.js
+++ esdoc-external-ecmascript-plugin/src/index.js
@@ -1,14 +1,9 @@
 const ExternalECMAScriptPlugin = require('./ExternalECMAScriptPlugin');
 
-let option;
 let plugin;
 
-exports.onStart = function(ev) {
-  option = ev.data.option;
-};
-
 exports.onHandleConfig = function(ev) {
-  plugin = new ExternalECMAScriptPlugin(ev.data.config, option);
+  plugin = new ExternalECMAScriptPlugin(ev.data.config, ev.data.option);
   plugin.exec();
 };
 
