18ff1b7d26e6583ef6b30eb42085aa9256be4c24
h13i32maru <h13i32maru@gmail.com>
2017-03-05 21:09:59 +0900

    fix(importpath): use new handler option

--- esdoc-importpath-plugin/src/index.js
+++ esdoc-importpath-plugin/src/index.js
@@ -1,17 +1,12 @@
 const ImportPathPlugin = require('./ImportPathPlugin');
 
-let option;
 let config;
-
-exports.onStart = function(ev) {
-  option = ev.data.option;
-};
 
 exports.onHandleConfig = function(ev) {
   config = ev.data.config;
 };
 
 exports.onHandleTag = function(ev) {
-  const plugin = new ImportPathPlugin(config, ev.data.tag, option);
+  const plugin = new ImportPathPlugin(config, ev.data.tag, ev.data.option);
   plugin.exec();
 };
