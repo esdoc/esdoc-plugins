6735aaa080d2c252fdc929ba29fecb84c14d1d33
h13i32maru <h13i32maru@gmail.com>
2017-05-14 16:18:50 +0900

    chore(script): better check script

--- script/check.sh
+++ script/check.sh
@@ -3,14 +3,11 @@
 for plugin in $(ls -1 -d esdoc-*)
 do
   cd $plugin
-  localDate=$(TZ=UTC git log --date=format-local:%Y-%m-%d -n 1 ./ | grep Date | cut -d ' ' -f 4 | cut -d 'T' -f 1)
-  npmDate=$(npm view ./ | grep modified | awk -F "'" '{print $2}' | cut -d 'T' -f 1)
-
-  if [ $localDate != $npmDate ]; then
+#  localDate=$(TZ=UTC git log --date=format-local:%Y-%m-%d -n 1 ./ | grep Date | cut -d ' ' -f 4 | cut -d 'T' -f 1)
+  npmDate=$(npm view ./ | grep modified | awk -F "'" '{print $2}')
+  log=$(git log --since="$npmDate" --invert-grep --grep="bump v" -n 1 ./)
+  if [ "$log" != "" ]; then
     echo $plugin
-    echo $localDate
-    echo $npmDate
   fi
-
   cd ../
 done
