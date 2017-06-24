513d465fe93bf2b48637b16cffa49e49834fd559
h13i32maru <h13i32maru@gmail.com>
2017-06-24 11:16:59 +0900

    refactor(external-ecmascript): structure

--- esdoc-external-ecmascript-plugin/test/src/MyClass.test.js
+++ esdoc-external-ecmascript-plugin/test/src/MyClass.test.js
@@ -1,19 +1,9 @@
 const assert = require('assert');
 const path = require('path');
 const fs = require('fs');
-const ESDocCLI = require('esdoc/out/src/ESDocCLI.js').default;
-
-function cli() {
-  const cliPath = path.resolve('./node_modules/esdoc/out/ESDocCLI.js');
-  const argv = ['node', cliPath, '-c', './test/fixture/esdoc.json'];
-  const cli = new ESDocCLI(argv);
-  cli.exec();
-}
-
-cli();
 
 describe('test external ecmascript results:', ()=>{
-  const tmp = fs.readFileSync('./test/fixture/out/index.json').toString();
+  const tmp = fs.readFileSync('./test/out/index.json').toString();
   const tags = JSON.parse(tmp);
 
   it('has external ecmascript.', ()=>{
@@ -23,7 +13,7 @@ describe('test external ecmascript results:', ()=>{
 
   it('removed external-ecmascript.js', ()=>{
     assert.throws(()=>{
-      fs.readFileSync('./test/fixture/src/.external-ecmascript.js');
+      fs.readFileSync('./test/src/.external-ecmascript.js');
     });
   });
 });
