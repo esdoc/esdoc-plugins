53b4c7726d51f40ec64c2b4d191329577c3c7e58
Jérôme HENAFF <jerome.henaff@amdocs.com>
2018-01-24 16:37:02 +0100

    add unit test for missing leading comment case rename test file to fit with current tests

---
+++ esdoc-lint-plugin/test/src/MyArrowFunction.js
@@ -0,0 +1,14 @@
+
+/**
+ * this is arrowFunction1 with a good import
+ * @param p1
+ * @param p2
+ */
+export const arrowFunction1 = (p1, p2) => {};
+
+/**
+ * this is arrowFunction2 with a bad import
+ * necessary to tests the missing leadingComments
+ * @param p1
+ */
+export const arrowFunction2 = (p1, p2) => {};
