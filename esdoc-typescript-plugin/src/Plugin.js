ff0ba1c5a1cdc61e5c7ad4e9ec277a31a83abf31
Sebastian Wessel <sebastianwessel@users.noreply.github.com>
2019-04-28 00:34:26 +0200

    Fix typescript and proposal support

--- esdoc-typescript-plugin/src/Plugin.js
+++ esdoc-typescript-plugin/src/Plugin.js
@@ -22,8 +22,30 @@ class Plugin {
   onHandleCodeParser(ev) {
     const option = ev.data.option;
     const plugins = ev.data.parserOption.plugins;
-
+    
+    plugins.push('@babel/plugin-proposal-export-default-from')
+    plugins.push('@babel/plugin-proposal-class-properties')
+    plugins.push('@babel/plugin-proposal-object-rest-spread')
+    plugins.push('@babel/plugin-proposal-do-expressions')
+    plugins.push('babel-plugin-transform-function-bind')
+    plugins.push('@babel/plugin-proposal-function-sent')
+    plugins.push('@babel/plugin-syntax-async-generators')
+    plugins.push(['@babel/plugin-proposal-decorators',{ decoratorsBeforeExport: false }])
+    plugins.push('@babel/plugin-syntax-export-extensions')
+    plugins.push('@babel/plugin-syntax-dynamic-import')
+    plugins.push('@babel/plugin-syntax-typescript')
+    plugins.push('exportDefaultFrom')
     plugins.push('typescript')
+    plugins.push('classProperties');
+    plugins.push('objectRestSpread');
+    plugins.push('doExpressions');
+    plugins.push('functionBind');
+    plugins.push('functionSent');
+    plugins.push('asyncGenerators');
+    plugins.push(['decorators', { decoratorsBeforeExport: false }]);
+    plugins.push('exportExtensions');
+    plugins.push('dynamicImport');
+    
   }
 }
 
