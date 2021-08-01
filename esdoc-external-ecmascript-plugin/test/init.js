ab7735ae41bebcde2925a226a9c07e7e8c7ebcc3
Shubham Verma <shubhamv8527@gmail.com>
2021-08-01 19:20:06 +0530

    updated ecmascript plugin

--- esdoc-external-ecmascript-plugin/test/init.js
+++ esdoc-external-ecmascript-plugin/test/init.js
@@ -1,9 +1,9 @@
 const path = require('path');
 const fs = require('fs');
-const ESDocCLI = require('esdoc/out/src/ESDocCLI.js').default;
+const ESDocCLI = require('esdoc-latest/out/src/ESDocCLI.js').default;
 
 function cli() {
-  const cliPath = path.resolve('./node_modules/esdoc/out/ESDocCLI.js');
+  const cliPath = path.resolve('./node_modules/esdoc-latest/out/ESDocCLI.js');
   const argv = ['node', cliPath, '-c', './test/esdoc.json'];
   const cli = new ESDocCLI(argv);
   cli.exec();
