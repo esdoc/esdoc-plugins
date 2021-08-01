0e935f325bf8602115fe27d4cb94b897b4d8468d
Shubham Verma <shubhamv8527@gmail.com>
2021-08-01 19:46:17 +0530

    updated publish html package

--- esdoc-publish-html-plugin/test/src/init.js
+++ esdoc-publish-html-plugin/test/src/init.js
@@ -2,7 +2,7 @@ const path = require('path');
 const ESDocCLI = require('esdoc-latest/out/src/ESDocCLI.js').default;
 
 function cli() {
-  const cliPath = path.resolve('./node_modules/esdoc-latest/out/src/ESDocCLI.js');
+  const cliPath = path.resolve('./node_modules/esdoc-latest/out/ESDocCLI.js');
   const argv = ['node', cliPath, '-c', './test/fixture/esdoc.json'];
   const cli = new ESDocCLI(argv);
   cli.exec();
